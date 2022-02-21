622b6918624370b5e2c7b30b1c1b5253d8acbbbd
Trongnguyen <trongnguyenhuy7@gmail.com>
2022-02-21 09:28:17 +0700

    Commit 10

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import { STAFFS, DEPARTMENTS } from './shared/staffs';
 import StaffList from './components/StaffListComponent';
+import NavbarComponent from './components/NavbarComponent';
+
 
 class App extends Component {
 
@@ -17,12 +18,8 @@ class App extends Component {
     return (
       <div className="App"
         style={{ backgroundImage: 'url(./assets/images/employeeCard.jpg)' }}>
-        <div style={{ backgroundColor: 'rgba(0,0,0,0.7)', minheight: '2000px'}}>
-          <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-            </div>
-          </Navbar>
+        <div style={{ backgroundColor: 'rgba(0,0,0,0.7)', minheight: '2000px' }}>
+          <NavbarComponent />
           <StaffList staffs={this.state.staffs} department={this.state.department} />
         </div>
       </div>
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -6,7 +6,6 @@ class StaffList extends Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
             selectStaff: null
         }
@@ -20,8 +19,8 @@ class StaffList extends Component {
         if (staff != null) {
             return (
                 <Card className="mt-2 border border-dark">
-                    <CardHeader><b>Họ và tên: {staff.name}</b></CardHeader>
-                    <CardBody>
+                    <CardHeader className="bg-info"><b>Họ và tên: {staff.name}</b></CardHeader>
+                    <CardBody className="bg-success">
                         <CardText>Ngày Sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
                         <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
                         <CardText>Phòng Ban: {staff.department.name}</CardText>
@@ -33,45 +32,49 @@ class StaffList extends Component {
         } else {
             return (
                 <div>
-                    <p>Bấm vào tên nhân viên để xem thông tin</p>
+                    <div className="card mt-2 bg-danger text-center">
+                        <div className="card-body">
+                            <h4 className="card-title">Bấm vào tên nhân viên để xem thông tin</h4>
+                        </div>
+                    </div>
                 </div>
             );
         }
     }
 
     renderListStaff = () => {
-            return this.props.staffs.map((staff) => {
-                return (
-                    <div className="col-12 col-md-4 col-lg-2 mt-1"
-                        key={staff.id}>
-                        <div className="card border border-dark"
-                            onClick={() => this.onStaffSelect(staff)}
-                            style={{ cursor: 'pointer', height: '300px', width: '100%' }}>
-                            <img className="card-img-top" src={staff.image} alt={staff.name}
-                                style={{ width: '100%' }} />
-                            <div className="card-body bg-secondary p-2">
-                                <h4 className="card-title text-center">{staff.name}</h4>
-                                <span className="card-text">
-                                    Department: {staff.department.name}
-                                </span>
-                            </div>
+        return this.props.staffs.map((staff) => {
+            return (
+                <div className="col-12 col-md-4 col-lg-2 mt-2"
+                    key={staff.id}>
+                    <div className="card border border-dark"
+                        onClick={() => this.onStaffSelect(staff)}
+                        style={{ cursor: 'pointer', height: '300px', width: '100%' }}>
+                        <img className="card-img-top" src={staff.image} alt={staff.name}
+                            style={{ width: '100%' }} />
+                        <div className="card-body bg-secondary p-2">
+                            <h4 className="card-title text-center">{staff.name}</h4>
+                            <span className="card-text">
+                                Department: {staff.department.name}
+                            </span>
                         </div>
                     </div>
-                );
-            });
+                </div>
+            );
+        });
     }
 
     render() {
 
         return (
             <div className="container">
-                <div className="row">
-                    {this.renderListStaff()}
-                </div>
                 <div className="row justify-content-center">
                     <div className="col-12 col-md-6 m1-1">
                         {this.renderStaff(this.state.selectStaff)}
                     </div>
+                </div>
+                <div className="row">
+                    {this.renderListStaff()}
                 </div>
             </div>
         );
